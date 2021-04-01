4d47039666596c4a4829baf16e4cffda00831feb
Hassan Abbas <hazzyhassan888@gmail.com>
2021-04-01 17:28:47 +0500

    empty

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
+
+const connectDb = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Mongo Connected.....");
+  } catch (error) {
+    console.error(error.message);
+
+    // Exit Process with failiure
+    process.exit(1);
+  }
+};
+module.exports = connectDb;
