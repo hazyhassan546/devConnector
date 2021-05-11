de716d6bfc05d90b4620a98dda2e76e4e102979f
Hassan Abbas <hazzyhassan888@gmail.com>
2021-05-11 12:52:19 +0500

    FE REmoved

--- client/src/views/auth/login/index.js
+++
@@ -1,3 +0,0 @@
-// import Login from "./Login";
-// import { globalConnect } from "../../../redux/connect/globalConnect";
-// export default globalConnect()(Login);
--- client/src/views/auth/register/index.js
+++
@@ -1,3 +0,0 @@
-import Register from "./Register";
-import { globalConnect } from "../../../redux/connect/globalConnect";
-export default globalConnect()(Register);
